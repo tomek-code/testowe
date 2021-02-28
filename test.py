faf155f4bd327f638927340015e61899775a0731
Tomek <t.sikor4@gmail.com>
2021-02-28 12:39:31 +0100

    complete python course

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 
 
-def my_function():
-    print("this is my function!")
-    print("test154")
+def my_function(str1, str2):
+    print(str1)
+    print(str2)
 
 
-my_function()
+my_function("this is argument 1", "this is second argument")
+my_function("strings", "hello world")
