12b2dc72ceafb274519ba3dd9c9df474b4d2ce68
tomek <t.sikor4@gmail.com>
2021-02-28 22:16:28 +0100

    commit

---
+++ automateboring01.py
@@ -0,0 +1,11 @@
+#first program 
+
+print("hello world!")
+print("whats your name?") # ask for name
+myName = input()
+print("nice to mee you, " + myName)
+print("the lenght of your name is : ")
+print(len(myName))
+print("whats youre age?")
+myAge = input()
+print ("you will be ", str(int(myAge) + 1 ) + "in this year")
